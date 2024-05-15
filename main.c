9ea1fa63bf7501b07c7d28c69a5334377669ee6c
O.R <orezek@seznam.cz>
2024-05-15 22:19:38 +0200

    Initial project commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 22:12:18 by aldokezer         #+#    #+#             */
+/*   Updated: 2024/05/15 22:19:12 by aldokezer        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "philos.h"
+
+//cc main.c -Wall -Wextra -Werror -o philos && ./philos
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("Hello Philosophers!\n");
+	return (0);
+}
