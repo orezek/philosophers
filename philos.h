d11a0f077fb624f1760b9aaf82ee121cd5c75ff0
O.R <orezek@seznam.cz>
2024-05-16 21:45:09 +0200

    Update: add libraries to the h file

--- philos.h
+++ philos.h
@@ -6,10 +6,13 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 22:13:16 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/05/15 22:16:50 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/05/16 21:44:27 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <limits.h>
+# include <pthread.h>
+# include <sys/time.h>
