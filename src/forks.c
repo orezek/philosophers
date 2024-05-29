fb3ac486b9a635b73b92946e83fcbff54e01e844
O.R <orezek@seznam.cz>
2024-05-29 21:45:34 +0200

    Update: forks and thread_states

--- src/forks.c
+++ src/forks.c
@@ -6,22 +6,22 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 08:48:25 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/05/29 21:34:43 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/05/29 21:44:40 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philos.h"
 
-int	ft_lock_forks(t_philosopher *philosophers)
+int	ft_lock_forks(t_philosopher *philosopher)
 {
-	(void)philosophers;
-	printf("Lock forks\n");
+	(void)philosopher;
+	printf("Phil: %d Locks\n", philosopher->id);
 	return(0);
 }
 
-int ft_release_forks(t_philosopher *philosophers)
+int ft_release_forks(t_philosopher *philosopher)
 {
-	(void)philosophers;
-	printf("Unlock forks\n");
+	(void)philosopher;
+	printf("Phil: %d Unlocks\n", philosopher->id);
 	return(0);
 }
--- src/thread_states.c
+++ src/thread_states.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 08:43:15 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/05/29 21:38:08 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/05/29 21:42:16 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 int ft_eat_state(t_philosopher *philosopher)
 {
 	(void)philosopher;
-	printf("Eating\n");
-	//ft_sleep(100);
+	printf("Phil: %d is Eating\n", philosopher->id);
+	ft_sleep(100);
 	return(0);
 }
 
 int ft_sleep_state(t_philosopher *philosopher)
 {
 	(void)philosopher;
-	printf("Sleeping\n");
-	//ft_sleep(100);
+	printf("Phil: %d is Sleeping\n", philosopher->id);
+	ft_sleep(100);
 	return(0);
 }
 
 int ft_think_state(t_philosopher *philosopher)
 {
 	(void)philosopher;
-	printf("Thinking\n");
-	//ft_sleep(100);
+	printf("Phil: %d is Thinking\n", philosopher->id);
+	ft_sleep(100);
 	return(0);
 }
