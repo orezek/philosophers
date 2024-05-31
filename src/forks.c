507dbcee232854646131be4861e264f9472d49cc
O.R <orezek@seznam.cz>
2024-05-31 11:57:38 +0200

    Fix mutexes

--- src/forks.c
+++ src/forks.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 08:48:25 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/05/31 01:18:51 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/05/31 11:37:34 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,48 @@ static int ft_get_right_fork_index(t_philosopher *p)
 	return (p->id + 1) % p->resources->n_of_philosophers;
 }
 
-static void ft_print_state(t_philosopher *p, char *message)
+// static void ft_print_state(t_philosopher *p, char *message)
+// {
+// 	long philosopher_eat_start;
+// 	philosopher_eat_start = ft_get_sim_elapased_time(p->resources->sim_start_time);
+// 	printf("%-10ld%-6d%s\n", philosopher_eat_start, p->id, message);
+// }
+
+int ft_lock_forks(t_philosopher *philosopher)
 {
-	long philosopher_eat_start;
-	philosopher_eat_start = ft_get_sim_elapased_time(p->resources->sim_start_time);
-	printf("%-10ld%-6d%s\n", philosopher_eat_start, p->id, message);
+	int left_fork_index = philosopher->id;
+	int right_fork_index = ft_get_right_fork_index(philosopher);
+
+	// Enforce lower index first
+	if (left_fork_index < right_fork_index)
+	{
+		ft_lock_left_fork(philosopher);
+		if (philosopher->left_fork == 1)
+			ft_lock_right_fork(philosopher);
+	}
+	else
+	{
+		ft_lock_right_fork(philosopher);
+		if (philosopher->right_fork == 1)
+			ft_lock_left_fork(philosopher);
+	}
+	return 0;
 }
 
-int	ft_lock_forks(t_philosopher *philosopher)
-{
-	if ((philosopher->id) % 2)
-		ft_lock_right_fork(philosopher);
-	else
-		ft_lock_left_fork(philosopher);
-	if (philosopher->left_fork == 1)
-		ft_lock_right_fork(philosopher);
-	else if (philosopher->right_fork == 1)
-		ft_lock_left_fork(philosopher);
-	return(0);
-}
+
+
+// int	ft_lock_forks(t_philosopher *philosopher)
+// {
+// 	if ((philosopher->id) % 2)
+// 		ft_lock_right_fork(philosopher);
+// 	else
+// 		ft_lock_left_fork(philosopher);
+// 	if (philosopher->left_fork == 1)
+// 		ft_lock_right_fork(philosopher);
+// 	else if (philosopher->right_fork == 1)
+// 		ft_lock_left_fork(philosopher);
+// 	return(0);
+// }
 
 
 
@@ -53,8 +76,8 @@ int ft_release_forks(t_philosopher *p)
 	p->resources->forks[ft_get_right_fork_index(p)] = AVAILABLE;
 	pthread_mutex_unlock(&p->resources->forks_mtxs[ft_get_right_fork_index(p)]);
 	// test
-	pthread_mutex_lock(&p->resources->print_console_mtx);
-	ft_print_state(p, "Forks released");
-	pthread_mutex_unlock(&p->resources->print_console_mtx);
+	// pthread_mutex_lock(&p->resources->print_console_mtx);
+	// ft_print_state(p, "Forks released");
+	// pthread_mutex_unlock(&p->resources->print_console_mtx);
 	return(0);
 }
